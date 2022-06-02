01641a144d2ac0242a408132a18e456b58274551
Artem Oleshkevich <oleshkevich.artem.1998@gmail.com>
2022-06-02 13:12:31 +0300

    Create project FileManager

---
+++ FileManager/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  FileManager
+//
+//  Created by Oleshkevich Artem on 2.06.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
